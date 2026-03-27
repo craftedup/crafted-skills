5910e441ff3215534979cfedba23aa6f4313f507
Ken Stark <ken@craftedup.com>
2026-03-27 15:03:19 -0400

    basepath fix

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -6,6 +6,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/crafted-skills",
   outputFileTracingRoot: path.join(__dirname, ".."),
   images: {
     unoptimized: true, // Required for static export
